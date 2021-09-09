59d8c6bf55bab21cd72de1053b2b4ecbc560e8c4
HiroUrata <1712026@i.softbank.jp>
2021-09-09 15:35:12 +0900

    ViewController　UI作成　tapDetection 追加

--- ScreenTapDetection/ViewController.swift
+++ ScreenTapDetection/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var DetectionResultLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let tapDetection = UITapGestureRecognizer(target: self, action: #selector(screenTap))
+        view.addGestureRecognizer(tapDetection)
+        
     }
 
+    @objc func screenTap(){
+        
+        
+    }
 
 }
 
