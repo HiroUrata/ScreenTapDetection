47c79baf712ba7e317f3ae8ae86c67e939461751
HiroUrata <1712026@i.softbank.jp>
2021-09-09 16:33:44 +0900

    タップ時の処理を追加

--- ScreenTapDetection/ViewController.swift
+++ ScreenTapDetection/ViewController.swift
@@ -12,8 +12,13 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var detectionResultLabel: UILabel!
     
+    var gradient = CAGradientLayer()
+    var tapCount = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        gradient.frame = CGRect(x: self.view.frame.minX, y: self.view.frame.minY, width: self.view.frame.size.width, height: self.view.frame.size.height)
         
         let tapDetection = UITapGestureRecognizer(target: self, action: #selector(screenTap))
         view.addGestureRecognizer(tapDetection)
@@ -22,7 +27,24 @@ class ViewController: UIViewController {
 
     @objc func screenTap(){
         
-        detectionResultLabel.text = "画面をタップしました"
+        tapCount += 1
+        detectionResultLabel.text = "\(tapCount)回、画面をタップしました"
+        
+        gradient.colors = [
+
+                        UIColor(red: CGFloat.random(in: 0...255) / 255,
+                                green:CGFloat.random(in:0...255) / 255,
+                                blue: CGFloat.random(in: 0...255) / 255,
+                                alpha: CGFloat.random(in: 0.0...1.0)).cgColor,
+                        UIColor(red: CGFloat.random(in: 0...255) / 255,
+                                green:CGFloat.random(in: 0...255) / 255,
+                                blue: CGFloat.random(in: 0...255) / 255,
+                                alpha: CGFloat.random(in: 0.0...1.0)).cgColor
+                    ]
+
+                    gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
+                    gradient.endPoint = CGPoint(x: 0.5, y: 0)
+                    self.view.layer.insertSublayer(gradient, at: 0)
         
     }
 
