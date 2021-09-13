5e5225bb68634f805a358ff13e592340d780e614
HiroUrata <1712026@i.softbank.jp>
2021-09-13 23:24:57 +0900

    screenTap　処理を変更

--- ScreenTapDetection/ViewController.swift
+++ ScreenTapDetection/ViewController.swift
@@ -52,35 +52,38 @@ class ViewController: UIViewController {
 
     @objc func screenTap(){
         
-        if tapCount <= 5{
+        if tapCount == 5{
+            
+            self.view.isUserInteractionEnabled = false
+            
+            
+        }else{
+            
+            
             print("screenTap")
             tapCount += 1
             detectionResultLabel.text = "\(tapCount)回、画面をタップしました"
             
             gradient.colors = [
-
-                            UIColor(red: CGFloat.random(in: 0...255) / 255,
-                                    green:CGFloat.random(in:0...255) / 255,
-                                    blue: CGFloat.random(in: 0...255) / 255,
-                                    alpha: CGFloat.random(in: 0.0...1.0)).cgColor,
-                            UIColor(red: CGFloat.random(in: 0...255) / 255,
-                                    green:CGFloat.random(in: 0...255) / 255,
-                                    blue: CGFloat.random(in: 0...255) / 255,
-                                    alpha: CGFloat.random(in: 0.0...1.0)).cgColor
-                        ]
-
-                        gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
-                        gradient.endPoint = CGPoint(x: 0.5, y: 0)
-                        self.view.layer.insertSublayer(gradient, at: 0)
+                
+                UIColor(red: CGFloat.random(in: 0...255) / 255,
+                        green:CGFloat.random(in:0...255) / 255,
+                        blue: CGFloat.random(in: 0...255) / 255,
+                        alpha: CGFloat.random(in: 0.0...1.0)).cgColor,
+                UIColor(red: CGFloat.random(in: 0...255) / 255,
+                        green:CGFloat.random(in: 0...255) / 255,
+                        blue: CGFloat.random(in: 0...255) / 255,
+                        alpha: CGFloat.random(in: 0.0...1.0)).cgColor
+            ]
             
-        }else if tapCount > 5{
-            
-            self.view.isUserInteractionEnabled = false
+            gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
+            gradient.endPoint = CGPoint(x: 0.5, y: 0)
+            self.view.layer.insertSublayer(gradient, at: 0)
         }
         
-        
-        
     }
+        
+    
 
     @objc func screenLongTap(){
         print("screenLongTap")
