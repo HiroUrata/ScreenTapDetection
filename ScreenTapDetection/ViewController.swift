c0449c5b162e2536f50837c247f6f0978e88b3ae
HiroUrata <1712026@i.softbank.jp>
2021-09-13 23:03:06 +0900

    tapCountによっては isUserInteractionEnabled = false 追加

--- ScreenTapDetection/ViewController.swift
+++ ScreenTapDetection/ViewController.swift
@@ -45,32 +45,40 @@ class ViewController: UIViewController {
         upSwipeDetection.direction = .up
         view.addGestureRecognizer(upSwipeDetection)
         
-        self.view.isUserInteractionEnabled = false
+        //self.view.isUserInteractionEnabled = false
         
     }
 
 
     @objc func screenTap(){
         
-        print("screenTap")
-        tapCount += 1
-        detectionResultLabel.text = "\(tapCount)回、画面をタップしました"
+        if tapCount <= 5{
+            print("screenTap")
+            tapCount += 1
+            detectionResultLabel.text = "\(tapCount)回、画面をタップしました"
+            
+            gradient.colors = [
+
+                            UIColor(red: CGFloat.random(in: 0...255) / 255,
+                                    green:CGFloat.random(in:0...255) / 255,
+                                    blue: CGFloat.random(in: 0...255) / 255,
+                                    alpha: CGFloat.random(in: 0.0...1.0)).cgColor,
+                            UIColor(red: CGFloat.random(in: 0...255) / 255,
+                                    green:CGFloat.random(in: 0...255) / 255,
+                                    blue: CGFloat.random(in: 0...255) / 255,
+                                    alpha: CGFloat.random(in: 0.0...1.0)).cgColor
+                        ]
+
+                        gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
+                        gradient.endPoint = CGPoint(x: 0.5, y: 0)
+                        self.view.layer.insertSublayer(gradient, at: 0)
+            
+        }else if tapCount > 5{
+            
+            self.view.isUserInteractionEnabled = false
+        }
         
-        gradient.colors = [
-
-                        UIColor(red: CGFloat.random(in: 0...255) / 255,
-                                green:CGFloat.random(in:0...255) / 255,
-                                blue: CGFloat.random(in: 0...255) / 255,
-                                alpha: CGFloat.random(in: 0.0...1.0)).cgColor,
-                        UIColor(red: CGFloat.random(in: 0...255) / 255,
-                                green:CGFloat.random(in: 0...255) / 255,
-                                blue: CGFloat.random(in: 0...255) / 255,
-                                alpha: CGFloat.random(in: 0.0...1.0)).cgColor
-                    ]
-
-                    gradient.startPoint = CGPoint(x: 0.5, y: 0.5)
-                    gradient.endPoint = CGPoint(x: 0.5, y: 0)
-                    self.view.layer.insertSublayer(gradient, at: 0)
+        
         
     }
 
